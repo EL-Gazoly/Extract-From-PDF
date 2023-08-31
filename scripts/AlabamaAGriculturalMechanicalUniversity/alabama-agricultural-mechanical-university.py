1ebf91255cacd1370131f620601fbe6dee48a398
EL-Gazoly <elgazoly11@gmail.com>
2023-08-31 15:07:41 +0300

    rename the cols and the files

--- scripts/AlabamaAGriculturalMechanicalUniversity/alabama-agricultural-mechanical-university.py
+++ scripts/AlabamaAGriculturalMechanicalUniversity/alabama-agricultural-mechanical-university.py
@@ -2,7 +2,7 @@ import re
 import csv
 
 # Open the text file
-with open('./AlabamaAGriculturalMechanicalUniversity.txt', 'r', encoding='utf-8') as txt_file:
+with open('./alabama-agricultural-mechanical-university.txt', 'r', encoding='utf-8') as txt_file:
     txt_content = txt_file.read()
 
     # Create a regex pattern to match course data
@@ -22,11 +22,11 @@ with open('./AlabamaAGriculturalMechanicalUniversity.txt', 'r', encoding='utf-8'
         course_data.append((course_code, course_title, credit_hours, description))
 
 # Write the extracted course data to a CSV file
-with open('./AlabamaAGriculturalMechanicalUniversity.csv', 'w', newline='', encoding='utf-8') as csv_file:
+with open('./alabama-agricultural-mechanical-university.csv', 'w', newline='', encoding='utf-8') as csv_file:
     csv_writer = csv.writer(csv_file)
 
     # Write header row
-    csv_writer.writerow(['Course Code', 'Course Title', 'Credit Hours', 'Description'])
+    csv_writer.writerow(['Code', 'Title', 'Hours', 'Description'])
 
     # Write course data rows
     csv_writer.writerows(course_data)
