8a18d884d23edf231accb7b71f884b3890c1f3f3
EL-Gazoly <elgazoly11@gmail.com>
2023-08-31 14:40:16 +0300

    rename the files and cols

--- scripts/Johnson University Undergraduate Academic/johnson-university-undergraduate-academic.py
+++ scripts/Johnson University Undergraduate Academic/johnson-university-undergraduate-academic.py
@@ -2,7 +2,7 @@ import re
 import csv
 
 # Open the text file
-with open('JohnsonUniversityUndergraduateAcademic.txt', 'r', encoding='utf-8') as txt_file:
+with open('johnson-university-undergraduate-academic.txt', 'r', encoding='utf-8') as txt_file:
     txt_content = txt_file.read()
 
     # Create a regex pattern to match course data
@@ -15,7 +15,7 @@ with open('JohnsonUniversityUndergraduateAcademic.txt', 'r', encoding='utf-8') a
     course_data.extend(matches)
 
 # Write the extracted course data to a CSV file
-with open('JohnsonUniversityUndergraduateAcademic.csv', 'w', newline='', encoding='utf-8') as csv_file:
+with open('johnson-university-undergraduate-academic.csv', 'w', newline='', encoding='utf-8') as csv_file:
     csv_writer = csv.writer(csv_file)
 
     # Write header row
@@ -25,4 +25,4 @@ with open('JohnsonUniversityUndergraduateAcademic.csv', 'w', newline='', encodin
     for course in course_data:
         csv_writer.writerow(course)
 
-print("CSV file 'JohnsonUniversityUndergraduateAcademic.csv' has been created with the extracted course data.")
+print("CSV file 'johnson-university-undergraduate-academic.csv' has been created with the extracted course data.")
