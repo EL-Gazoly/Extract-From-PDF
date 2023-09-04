9729ee4d2a5d06dc8c6ab5c7c10f69782d012123
EL-Gazoly <elgazoly11@gmail.com>
2023-09-04 19:33:17 +0300

    enahce the regex and rename the cols

--- scripts/spring arbor/spring-arbor.py
+++ scripts/spring arbor/spring-arbor.py
@@ -6,41 +6,32 @@ with open('./spring-arbor.txt', 'r', encoding='utf-8') as txt_file:
     txt_content = txt_file.read()
 
     # Create a regex pattern to match course data
-    pattern = re.compile(r'([A-Z]{3,4} \d{3})\s+(.*?)\s+\(([\d\-]+)\)\s+\(([^)]+)\)\s+(.*?)(?=(?:[A-Z]{3,4} \d{3})|\Z)', re.DOTALL)
-    
-    # Initialize a set to store course codes
-    processed_courses = set()
-    
+    pattern = re.compile(r'(\w+\s\d+[A-Z]*\*?|\w+\s\d{3}\*?)\s(.*?)(?:\s\((\d+)\))\s([\s\S]*?)(?=\n\w+\s\d+[A-Z]*\*?\s|\Z)')
+
     # Initialize a list to store extracted course data
     course_data = []
 
     # Find all matches in the text content
     matches = pattern.findall(txt_content)
+
     for match in matches:
-        course_code = match[0]
-        
-        # Check if the course code has already been processed
-        if course_code not in processed_courses:
-            processed_courses.add(course_code)
-            
-            course_name = match[1]
-            course_credit_hours = match[2]
-            course_term = match[3]
-            course_description = match[4]
-            
-            course_info = {
-                'code': course_code,
-                'name': course_name,
-                'credit': course_credit_hours,
-                'description': course_term + ' ' + course_description.strip()
-            }
-            
-            course_data.append(course_info)
+        code, title, credits, description = match
+        course_data.append({
+            'code': code.strip(),
+            'name': title.strip(),
+            'credits': int(credits),
+            'description': description.strip()
+        })
 
 # Write the extracted course data to a CSV file
 with open('spring-arbor.csv', 'w', newline='', encoding='utf-8') as csv_file:
-    csv_writer = csv.DictWriter(csv_file, fieldnames=['code', 'name', 'credit', 'description'])
-    csv_writer.writeheader()
-    csv_writer.writerows(course_data)
+    csv_writer = csv.writer(csv_file)
 
-print("CSV file 'spring-arbor.csv' has been created with the extracted course data.")
+    # Write header row
+    csv_writer.writerow(['code', 'name', 'credits', 'description'])
+
+    # Write course data rows
+    for course in course_data:
+        csv_writer.writerow([course['code'], course['name'], course['credits'], course['description']])
+
+print("CSV file 'spring-arbor' has been created with the extracted course data.")
