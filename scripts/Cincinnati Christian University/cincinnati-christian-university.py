762c0316fc9d9d9a87a284b97877d10a1ce6d62a
EL-Gazoly <elgazoly11@gmail.com>
2023-09-04 20:44:37 +0300

    rename the cols and enhance the regex

--- scripts/Cincinnati Christian University/cincinnati-christian-university.py
+++ scripts/Cincinnati Christian University/cincinnati-christian-university.py
@@ -6,7 +6,7 @@ with open('./cincinnati-christian-university.txt', 'r', encoding='utf-8') as txt
     txt_content = txt_file.read()
 
     # Create a regex pattern to match course data
-    pattern = re.compile(r'([A-Z]+(?: [A-Z]+)? \d+(?:–\d+)?(?:/[A-Z]+(?: [A-Z]+)? \d+(?:–\d+)?)?)\s+([^\(\n]+)\s+\(([^)]+)\)\s+([\s\S]+?)(?=\n[A-Z]+(?: [A-Z]+)? \d+(?:–\d+)?(?:/[A-Z]+(?: [A-Z]+)? \d+(?:–\d+)?)?|$)')
+    pattern = re.compile(r'([A-Z]+(?: [A-Z]+)?(?: \d+(?:, \d+)+| & [A-Z]+(?: [A-Z]+)? \d+(?:, \d+)+| \d+[A-Za-z])?(?: \d+(?:–\d+)?(?:/[A-Z]+(?: [A-Z]+)? \d+(?:–\d+)?)?)?)\s+([^\(\n]+)\s+\(([^)]+)\)\s+([\s\S]+?)(?=\n[A-Z]+(?: [A-Z]+)? \d+(?:–\d+)?(?:/[A-Z]+(?: [A-Z]+)? \d+(?:–\d+)?)?|$)')
 
     # Initialize a list to store extracted course data
     course_data = []
@@ -16,7 +16,7 @@ with open('./cincinnati-christian-university.txt', 'r', encoding='utf-8') as txt
     for match in matches:
         code = match[0]
         name = match[1].strip()
-        hours = match[2].strip()
+        hours = match[2].strip() if match[2] else '0'  # Set to '0' if hours are not provided
         description = match[3].strip()
 
         # Store the extracted course data in a dictionary
@@ -30,12 +30,30 @@ with open('./cincinnati-christian-university.txt', 'r', encoding='utf-8') as txt
         # Append the course data to the list
         course_data.append(course_info)
 
+# Check if there are any courses in the description format
+# and extract them separately
+additional_courses = re.findall(r'([A-Z]+(?: [A-Z]+)?(?: \d+(?:, \d+)+| \d+[A-Za-z])?(?: \d+(?:–\d+)?(?:/[A-Z]+(?: [A-Z]+)? \d+(?:–\d+)?)?)?)\s+([^\(\n]+)', txt_content)
+for course in additional_courses:
+    code = course[0]
+    name = course[1].strip()
+
+    # Store the extracted course data in a dictionary
+    course_info = {
+        'Code': code,
+        'Title': name,
+        'Hours': '0',  # Set to '0' for courses without specified hours
+        'Description': ''
+    }
+
+    # Append the course data to the list
+    course_data.append(course_info)
+
 # Write the extracted course data to a CSV file
 with open('cincinnati-christian-university.csv', 'w', newline='', encoding='utf-8') as csv_file:
     csv_writer = csv.writer(csv_file)
 
     # Write header row
-    csv_writer.writerow(['Code', 'Title', 'Hours', 'Description'])
+    csv_writer.writerow(['code', 'title', 'hours', 'description'])
 
     # Write course data rows
     for course in course_data:
