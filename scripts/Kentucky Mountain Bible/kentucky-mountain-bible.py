dc332df97775302aeee2c25ea3c1bc9a7eecc0e7
EL-Gazoly <elgazoly11@gmail.com>
2023-09-05 13:48:04 +0300

    enhace the regex and rename the cols

--- scripts/Kentucky Mountain Bible/kentucky-mountain-bible.py
+++ scripts/Kentucky Mountain Bible/kentucky-mountain-bible.py
@@ -5,15 +5,27 @@ import csv
 with open('kentucky-mountain-bible.txt', 'r', encoding='utf-8') as txt_file:
     txt_content = txt_file.read()
 
-    # Create a regex pattern to match course data
-    pattern = re.compile(r'(\w{2,}\s\d{3}(?:-\d{3})?)\s(.*?)(?:\(([\d.]+)\scredits? (?:for each )?course\))?\n(.*?)(?=\n\w{2,}\s\d{3}(?:-\d{3})?|\Z|\n\(\d+ credit, required for graduation\))', re.DOTALL)
+    # Create a regex pattern to match the original course data
+    pattern1 = re.compile(r'(\w{2,}\s\d{3}(?:-\d{3})?)\s(.*?)(?:\((\d+)\scredits?(?:,?\srequired for graduation)?\))?\n(.*?)(?=\n\w{2,}\s\d{3}(?:-\d{3})?|\Z)', re.DOTALL)
 
     # Initialize a list to store extracted course data
     course_data = []
 
     # Find all matches in the text content
-    matches = pattern.findall(txt_content)
-    for match in matches:
+    matches1 = pattern1.findall(txt_content)
+    for match in matches1:
+        code = match[0]
+        name = match[1].strip()
+        credits = match[2] if match[2] else '0'  # Replace None with '0'
+        description = match[3].strip()
+        course_data.append({'Code': code, 'Title': name, 'Credits': credits, 'Description': description})
+
+    # Create a regex pattern to match the new course format, allowing '&' in the title
+    pattern2 = re.compile(r'(\w{2,}\s\d{3}(?:\s?and\s?\d{3})+)\s(.*?)(?:\((\d+)\scredits?(?:,?\srequired for graduation)?\))?\n(.*?)(?=\n\w{2,}\s\d{3}(?:-\d{3})?|\Z)', re.DOTALL)
+
+    # Find all matches in the text content
+    matches2 = pattern2.findall(txt_content)
+    for match in matches2:
         code = match[0]
         name = match[1].strip()
         credits = match[2] if match[2] else '0'  # Replace None with '0'
@@ -22,12 +34,20 @@ with open('kentucky-mountain-bible.txt', 'r', encoding='utf-8') as txt_file:
 
 # Write the extracted course data to a CSV file
 with open('kentucky-mountain-bible.csv', 'w', newline='', encoding='utf-8') as csv_file:
-    csv_writer = csv.DictWriter(csv_file, fieldnames=['Code', 'Title', 'Credits', 'Description'])
+    csv_writer = csv.DictWriter(csv_file, fieldnames=['code', 'title', 'credits', 'description'])
 
     # Write header row
     csv_writer.writeheader()
 
     # Write course data rows
-    csv_writer.writerows(course_data)
+    for course in course_data:
+        # Remove any extra characters from the title
+        course['Title'] = course['Title'].strip('"')
+        
+        # Remove any extra characters from the credits
+        course['Credits'] = course['Credits'].strip('"')
+        
+        # Write the modified course data
+        csv_writer.writerow(course)
 
 print("CSV file 'kentucky-mountain-bible.csv' has been created with the extracted course data.")
