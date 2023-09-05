0cb2e1158524bf0743a56970b7891c0c6c15e857
El-Gazoly <elgazoly11@gmail.com>
2023-09-06 00:38:30 +0300

    extract franklin

--- scripts/Franklin College/franklin-college.py
+++ scripts/Franklin College/franklin-college.py
@@ -6,7 +6,8 @@ with open('./franklin-college.txt', 'r', encoding='utf-8') as input_file:
     input_text = input_file.read()
 
 # Regular expression pattern
-pattern = re.compile(r'([A-Z\/\s\d]+?[A-Z\/]{3,}\s?\d{3,}[A-Z]*)(?:\s(.+?))?\n(\d+)\s*credit\s?hours?\s+([\s\S]+?)(?=\n[A-Z]{3,4}\s\d{3,4}|$)', re.IGNORECASE)
+pattern = re.compile(r'(\S+\s+\d+(?:\.\d+)?[A-Z]*[A-Z\d]*)\s+(.*?)\s+(\d+(?:-\d+)?|No\scredit)\s*(.*?)\s*(?:Prereq:\s(.*?)\s+)?(?:Coreq|Co-req:\s(.*?)\s+)?(except:\s(.*?)\s+)?\s*(.*?)(?=\n\S+\s+\d+|$)', re.DOTALL)
+
 # Find all matches in the input text
 matches = pattern.findall(input_text)
 
@@ -17,16 +18,29 @@ course_data = []
 for match in matches:
     course_code = match[0]
     course_name = match[1].strip()
-    credit_hours = match[2].strip()  # Only the number of credit hours
-    description = match[3].strip()
-    course_data.append([course_code, course_name, credit_hours, description])
+    credit_hours = match[2]
+    if credit_hours.lower() == "no credit":
+        credit_hours = "0"
+    prereq = match[4].strip() if match[4] else "None"
+    coreq = match[5].strip() if match[5] else "None"
+    except_info = match[7].strip() if match[7] else "None"
+    description = match[8].strip()
+
+    course_info = {
+        'code': course_code,
+        'title': course_name,
+        'credits': credit_hours,
+        'description': prereq+ '' + coreq+ ''+ except_info+ '' +description
+    }
+
+    course_data.append(course_info)
 
 # Write the extracted course data to a CSV file
 with open('franklin-college.csv', 'w', newline='', encoding='utf-8') as csv_file:
-    csv_writer = csv.writer(csv_file)
+    csv_writer = csv.DictWriter(csv_file, fieldnames=['code', 'title', 'credits','description'])
     
     # Write header row
-    csv_writer.writerow(['Code', 'Name', 'Credit', 'Description'])
+    csv_writer.writeheader()
     
     # Write course data rows
     csv_writer.writerows(course_data)
