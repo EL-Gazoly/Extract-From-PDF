8b3879376b8eae47f84f2b46cce3d442b65b2d81
EL-GAZOLY <elgazoly11@gmail.com>
2023-09-05 03:10:22 +0300

    enhance the regex and rename the cols

--- scripts/The Redlands Community College/the-redlands-community-college.py
+++ scripts/The Redlands Community College/the-redlands-community-college.py
@@ -5,8 +5,8 @@ import csv
 with open('./the-redlands-community-college.txt', 'r', encoding='utf-8') as txt_file:
     txt_content = txt_file.read()
 
-    # Create a regex pattern to match course data
-    pattern = re.compile(r'(\w+\s+\d+(?:-\d+)?\s*)(?:\s+(.+?))?\s+([\s\S]*?)((?=\w+\s+\d+(?:-\d+)?\s+)|$)')
+    # Create a regex pattern to match course data with various formats
+    pattern = re.compile(r'(\w+(?:\s+|\s?-)\d+(?:-\d+)?\s*)(?:\s+(.+?))?\s+([\s\S]*?)(?=\w+(?:\s+|\s?-)\d+(?:-\d+)?\s+|$)')
 
     # Initialize a list to store extracted course data
     course_data = []
@@ -14,22 +14,43 @@ with open('./the-redlands-community-college.txt', 'r', encoding='utf-8') as txt_
     # Find all matches in the text content
     matches = pattern.findall(txt_content)
 
-    for match in matches:
-        code = match[0].strip()
-        name = match[1].strip() if match[1] else ""
-        description = match[2].strip()
-        credit_hours = int(re.search(r'\d$', code).group())
+    # Initialize variables to store course details
+    code = ""
+    title = ""
+    description = ""
 
+    for match in matches:
+        match_code = match[0].strip()
+        match_name = match[1].strip() if match[1] else ""
+        match_description = match[2].strip()
+
+        # Check if this part of the match is a code or a title
+        if re.match(r'^\w+\s+\d+(?:-\d+)?$', match_code):
+            # If it's a code, update the previous course details and reset
+            if code:
+                course_data.append({
+                    'code': code,
+                    'title': title,
+                    'description': description
+                })
+            code = match_code
+            title = match_name
+            description = match_description
+        else:
+            # If it's not a code, append the description to the current course
+            description += "\n" + match_description
+
+    # Add the last course to the list
+    if code:
         course_data.append({
-            'Code': code,
-            'Title': name,
-            'Credits': credit_hours,
-            'Description': description
+            'code': code,
+            'title': title,
+            'description': description
         })
 
 # Write the extracted course data to a CSV file
 with open('the-redlands-community-college.csv', 'w', newline='', encoding='utf-8') as csv_file:
-    csv_writer = csv.DictWriter(csv_file, fieldnames=['Code', 'Title', 'Credits', 'Description'])
+    csv_writer = csv.DictWriter(csv_file, fieldnames=['code', 'title', 'description'])
 
     # Write header row
     csv_writer.writeheader()
