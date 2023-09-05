17b99b3af7b8fb299fa2dc74fb4644f61bf3d62a
El-Gazoly <elgazoly11@gmail.com>
2023-09-05 21:17:15 +0300

    rename cols and enhance the regex to handel prerequisite

--- scripts/Oakland City University/oakland-city-university.py
+++ scripts/Oakland City University/oakland-city-university.py
@@ -5,8 +5,8 @@ import csv
 with open('./oakland-city-university.txt', 'r', encoding='utf-8') as txt_file:
     txt_content = txt_file.read()
 
-    # Create a regex pattern to match course data and extract credit hours as numbers
-    pattern = re.compile(r'(\w+\s+\d+)\s+(.*?)\s+(\d+(?:-\d+)?)\s+(?:hour|hours)\s+(.*?)\s*(?=\w+\s+\d+|\Z)', re.DOTALL | re.IGNORECASE)
+    # Create a regex pattern to match course data and extract required fields
+    pattern = re.compile(r'(\w{1,4}\s\d{3})\s+([^\d]+)\s+(\d+(?:\.\d+)?(?:-\d+(?:\.\d+)?)?)\s+(.*?)\s*(?:Prerequisite: (.*?))?(?=\n\w{1,4}\s\d{3}|\Z)', re.DOTALL)
 
     # Initialize a list to store extracted course data
     course_data = []
@@ -16,13 +16,17 @@ with open('./oakland-city-university.txt', 'r', encoding='utf-8') as txt_file:
     for match in matches:
         code = match[0]
         title = match[1].strip()
-        credits = match[2].strip()
+        credits = match[2]
         description = match[3].strip()
+        prerequisite = match[4].strip() if match[4] else "None"
+
+        # Concatenate Prerequisite with the description
+        description += "\nPrerequisite: " + prerequisite if prerequisite != "None" else ""
 
         course_data.append({
             'code': code,
             'name': title,
-            'credit hours': credits,
+            'credits': credits,
             'description': description
         })
 
@@ -31,10 +35,10 @@ with open('oakland-city-university.csv', 'w', newline='', encoding='utf-8') as c
     csv_writer = csv.writer(csv_file)
 
     # Write header row
-    csv_writer.writerow(['code', 'name', 'credit hours', 'description'])
+    csv_writer.writerow(['code', 'name', 'credits', 'description'])
 
     # Write course data rows
     for course in course_data:
-        csv_writer.writerow([course['code'], course['name'], course['credit hours'], course['description']])
+        csv_writer.writerow([course['code'], course['name'], course['credits'], course['description']])
 
 print("CSV file 'oakland-city-university.csv' has been created with the extracted course data.")
