0dd8cbf1eec17aac7abca0560e3a447ea2a36d9d
EL-Gazoly <elgazoly11@gmail.com>
2023-09-05 14:36:44 +0300

    enhace the regex and rename the cols

--- scripts/Kentucky Mountain Bible/Extract pdf to text.py
+++ scripts/Kentucky Mountain Bible/Extract pdf to text.py
@@ -11,49 +11,60 @@ with open('kentucky-mountain-bible.txt', 'r', encoding='utf-8') as txt_file:
     # Initialize a list to store extracted course data
     course_data = []
 
+    # Initialize a set to store course codes
+    course_codes = set()
+
     # Find all matches in the text content
     matches1 = pattern1.findall(txt_content)
     for match in matches1:
         code = match[0]
-        name = match[1].strip()
-        credits = match[2] if match[2] else '0'  # Replace None with '0'
-        description = match[3].strip()
-        course_data.append({'Code': code, 'Title': name, 'Credits': credits, 'Description': description})
+        if code not in course_codes:
+            name = match[1].strip()
+            credits = match[2] if match[2] else '0'  # Replace None with '0'
+            description = match[3].strip()
+            course_data.append({'Code': code, 'Title': name, 'Credits': credits, 'Description': description})
+            course_codes.add(code)
 
     # Create a regex pattern to match the new course format
-    pattern2 = re.compile(r'(\w{2,}\s\d{3}(?:\s?and\s?\d{3})+)\s(.*?)\((\d+)\scredits?(?:,?\srequired for graduation)?\)\n(.*?)\n', re.DOTALL)
+    pattern2 = re.compile(r'(\w{2,}\s\d{3}(?:\s?and\s?\d{3})+)\s(.*?)\((\d+)\scredits?(?:\sfor each course)?\)\n(.*?)\n', re.DOTALL)
 
     # Find all matches in the text content
     matches2 = pattern2.findall(txt_content)
     for match in matches2:
         code = match[0]
-        name = match[1].strip()
-        credits = match[2]
-        description = match[3].strip()
-        course_data.append({'Code': code, 'Title': name, 'Credits': credits, 'Description': description})
+        if code not in course_codes:
+            title_parts = match[1].strip().split(',')
+            titles = [t.strip() for t in title_parts]
+            title = ', '.join(titles[:-1])
+            additional_info = titles[-1]
+            credits = match[2] if match[2] else '0'
+            description = match[3].strip()
+            course_data.append({'Code': code, 'Title': title, 'Additional Info': additional_info, 'Credits': credits, 'Description': description})
+            course_codes.add(code)
+
+    # Create a regex pattern to handle '&' in the course title
+    pattern3 = re.compile(r'(\w{2,}\s\d{3}(?:-\d{3})?)\s(.*?)(?:\(([\d.]+)\scredits?(?:,?\srequired for graduation)?\))?\n(.*?)(?=\n\w{2,}\s\d{3}(?:-\d{3})?|\Z)', re.DOTALL)
+
+    # Find all matches in the text content
+    matches3 = pattern3.findall(txt_content)
+    for match in matches3:
+        code = match[0]
+        if code not in course_codes:
+            name = match[1].strip()
+            credits = match[2] if match[2] else '0'
+            description = match[3].strip()
+            course_data.append({'Code': code, 'Title': name, 'Credits': credits, 'Description': description})
+            course_codes.add(code)
 
 # Write the extracted course data to a CSV file
 with open('kentucky-mountain-bible.csv', 'w', newline='', encoding='utf-8') as csv_file:
-    csv_writer = csv.DictWriter(csv_file, fieldnames=['Code', 'Title', 'Credits', 'Description'])
+    csv_writer = csv.DictWriter(csv_file, fieldnames=['Code', 'Title', 'Additional Info', 'Credits', 'Description'])
 
     # Write header row
     csv_writer.writeheader()
 
     # Write course data rows
     for course in course_data:
-        # Split the title by "(" to separate title and additional information
-        title_parts = course['Title'].split('(')
-        title = title_parts[0].strip()
-        
-        # Extract credits from the additional information
-        credits_info = title_parts[-1].strip(') ').split()
-        credits = credits_info[0]
-        
-        # Update the course data
-        course['Title'] = title
-        course['Credits'] = credits
-
-        # Write the modified course data
         csv_writer.writerow(course)
 
 print("CSV file 'kentucky-mountain-bible.csv' has been created with the extracted course data.")
--- scripts/Kentucky Mountain Bible/kentucky-mountain-bible.py
+++ scripts/Kentucky Mountain Bible/kentucky-mountain-bible.py
@@ -11,26 +11,35 @@ with open('kentucky-mountain-bible.txt', 'r', encoding='utf-8') as txt_file:
     # Initialize a list to store extracted course data
     course_data = []
 
+    # Initialize a set to store course codes
+    course_codes = set()
+
     # Find all matches in the text content
     matches1 = pattern1.findall(txt_content)
     for match in matches1:
         code = match[0]
-        name = match[1].strip()
-        credits = match[2] if match[2] else '0'  # Replace None with '0'
-        description = match[3].strip()
-        course_data.append({'Code': code, 'Title': name, 'Credits': credits, 'Description': description})
+        if code not in course_codes:
+            name = match[1].strip()
+            credits = match[2] if match[2] else '0'  # Replace None with '0'
+            description = match[3].strip()
+            course_data.append({'Code': code, 'Title': name, 'Credits': credits, 'Description': description})
+            course_codes.add(code)
 
     # Create a regex pattern to match the new course format
-    pattern2 = re.compile(r'(\w{2,}\s\d{3}(?:\s?and\s?\d{3})+)\s(.*?)(?:\((\d+)\scredits?(?:,?\srequired for graduation)?)\)\n(.*?)\n', re.DOTALL)
+    pattern2 = re.compile(r'(\w{2,}\s\d{3}(?:\s?and\s?\d{3})+)\s(.*?)\((\d+)\scredits?(?:\sfor each course)?\)\n(.*?)\n', re.DOTALL)
 
     # Find all matches in the text content
     matches2 = pattern2.findall(txt_content)
     for match in matches2:
         code = match[0]
-        name = match[1].strip()
-        credits = match[2] if match[2] else '0'  # Replace None with '0'
-        description = match[3].strip()
-        course_data.append({'Code': code, 'Title': name, 'Credits': credits, 'Description': description})
+        if code not in course_codes:
+            title_parts = match[1].strip().split('(')
+            titles = [t.strip() for t in title_parts[0].split('and')]
+            title = ', '.join(titles)
+            credits = match[2] if match[2] else '0'
+            description = match[3].strip()
+            course_data.append({'Code': code, 'Title': title, 'Credits': credits, 'Description': description})
+            course_codes.add(code)
 
     # Create a regex pattern to handle '&' in the course title
     pattern3 = re.compile(r'(\w{2,}\s\d{3}(?:-\d{3})?)\s(.*?)(?:\(([\d.]+)\scredits?(?:,?\srequired for graduation)?\))?\n(.*?)(?=\n\w{2,}\s\d{3}(?:-\d{3})?|\Z)', re.DOTALL)
@@ -39,10 +48,12 @@ with open('kentucky-mountain-bible.txt', 'r', encoding='utf-8') as txt_file:
     matches3 = pattern3.findall(txt_content)
     for match in matches3:
         code = match[0]
-        name = match[1].strip()
-        credits = match[2] if match[2] else '0'  # Replace None with '0'
-        description = match[3].strip()
-        course_data.append({'Code': code, 'Title': name, 'Credits': credits, 'Description': description})
+        if code not in course_codes:
+            name = match[1].strip()
+            credits = match[2] if match[2] else '0'
+            description = match[3].strip()
+            course_data.append({'Code': code, 'Title': name, 'Credits': credits, 'Description': description})
+            course_codes.add(code)
 
 # Write the extracted course data to a CSV file
 with open('kentucky-mountain-bible.csv', 'w', newline='', encoding='utf-8') as csv_file:
