5a5d453def2d2a3821813bd816ffedceccbbf9f1
El-Gazoly <elgazoly11@gmail.com>
2023-09-18 21:37:44 +0300

    extract oklahoma

---
+++ scripts/2nd Phase/Oklahoma State University/oklahoma-state-university.py
@@ -0,0 +1,37 @@
+import re
+import csv
+
+# Open the text file
+with open('./oklahoma-state-university.txt', 'r', encoding='utf-8') as txt_file:
+    txt_content = txt_file.read()
+
+    # Create a regex pattern to match course data
+    pattern = re.compile(r'(\w+\s+\d+)\s+(.*?)\s+\|\s+(\d+(?:-\d+)?)\s*(?:Credit\s+Hour|Hours)?\s+(.*?)\s+(?=(?:\w+\s+\d+|\Z))', re.DOTALL)
+
+    # Initialize a list to store extracted course data
+    course_data = []
+
+    # Find all matches in the text content
+    matches = pattern.findall(txt_content)
+
+    for match in matches:
+        code, title, credits, description = match
+        course_data.append({
+            'code': code.strip(),
+            'title': title.strip(),
+            'credits': credits.strip(),
+            'description': description.strip()
+        })
+
+# Write the extracted course data to a CSV file
+with open('oklahoma-state-university.csv', 'w', newline='', encoding='utf-8') as csv_file:
+    csv_writer = csv.writer(csv_file)
+
+    # Write header row
+    csv_writer.writerow(['code', 'title', 'credits', 'description'])
+
+    # Write course data rows
+    for course in course_data:
+        csv_writer.writerow([course['code'], course['title'], course['credits'], course['description']])
+
+print("CSV file 'oklahoma-state-university.csv' has been created with the extracted course data.")
