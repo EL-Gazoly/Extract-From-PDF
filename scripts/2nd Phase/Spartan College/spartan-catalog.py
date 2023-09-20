ed55656b4ac86e9b63c49a41c223b90ef1118dc1
El-Gazoly <elgazoly11@gmail.com>
2023-09-21 00:54:43 +0300

    trial

---
+++ scripts/2nd Phase/Spartan College/spartan-catalog.py
@@ -0,0 +1,37 @@
+import re
+import csv
+
+# Open the text file
+with open('./spartan-catalog.txt', 'r', encoding='utf-8') as txt_file:
+    txt_content = txt_file.read()
+
+    # Create a regex pattern to match course data
+    pattern = re.compile(r'(\w+)\s+-\s+(.*?)\s+\|\s+(\d+)\s+Semester Credit Hours.*?\n(.*?)\n\n', re.DOTALL)
+
+    # Initialize a list to store extracted course data
+    course_data = []
+
+    # Find all matches in the text content
+    matches = pattern.findall(txt_content)
+
+    for match in matches:
+        code, title, credits, description = match
+        course_data.append({
+            'code': code.strip(),
+            'title': title.strip(),
+            'credits': int(credits),
+            'description': description.strip()
+        })
+
+# Write the extracted course data to a CSV file
+with open('spartan-catalog.csv', 'w', newline='', encoding='utf-8') as csv_file:
+    csv_writer = csv.writer(csv_file)
+
+    # Write header row
+    csv_writer.writerow(['code', 'title', 'credits', 'description'])
+
+    # Write course data rows
+    for course in course_data:
+        csv_writer.writerow([course['code'], course['title'], course['credits'], course['description']])
+
+print("CSV file 'spartan-catalog' has been created with the extracted course data.")
