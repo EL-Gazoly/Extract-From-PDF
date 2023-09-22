ebda7ffd36058d57efb582042a4164b7ce23b370
El-Gazoly <elgazoly11@gmail.com>
2023-09-22 14:39:33 +0300

    extract the cameron

--- scripts/2nd Phase/Cameron University/cameron-university.py
+++ scripts/2nd Phase/Cameron University/cameron-university.py
@@ -5,7 +5,7 @@ import csv
 with open('cameron-university.txt', 'r', encoding='utf-8') as txt_file:
     txt_content = txt_file.read()
 
-    # Create a regex pattern to match course data with the enhanced formats
+    # Create a regex pattern to match course data with both old and enhanced formats
     pattern = re.compile(r'(\d+(?:-\d+)?\*?\w*)\s+([^0-9]+)\s+(\d+(?:-\d+)?)\s*(?:credit\s*hours)?\s+(.*?)(?=\d+(?:-\d+)?\*?|\Z)(?:\s+Prerequisites:\s(.*?))?(?:\s+Corequisite:\s(.*?))?', re.DOTALL)
 
     # Initialize a list to store extracted course data
@@ -34,9 +34,7 @@ with open('cameron-university.txt', 'r', encoding='utf-8') as txt_file:
             'code': code.strip(),
             'title': title.strip(),
             'credits': credits.strip(),
-            'description': description,
-            'prerequisites': prerequisites.strip(),
-            'corequisite': corequisite.strip()
+            'description': description + prerequisites.strip() + corequisite.strip(),
         })
 
 # Write the extracted course data to a CSV file
@@ -44,10 +42,10 @@ with open('cameron-university.csv', 'w', newline='', encoding='utf-8') as csv_fi
     csv_writer = csv.writer(csv_file)
 
     # Write header row
-    csv_writer.writerow(['code', 'title', 'credits', 'description', 'prerequisites', 'corequisite'])
+    csv_writer.writerow(['code', 'title', 'credits', 'description'])
 
     # Write course data rows
     for course in course_data:
-        csv_writer.writerow([course['code'], course['title'], course['credits'], course['description'], course['prerequisites'], course['corequisite']])
+        csv_writer.writerow([course['code'], course['title'], course['credits'], course['description']])
 
-print("CSV file 'cameron-university' has been created with the extracted course data.")
+print("CSV file 'cameron-university.csv' has been created with the extracted course data.")
