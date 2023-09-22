9c2d284b051e786b29507b789e11feb90333288d
El-Gazoly <elgazoly11@gmail.com>
2023-09-22 20:41:54 +0300

    screiner

--- scripts/2nd Phase/Schreiner University/schreiner-university.py
+++ scripts/2nd Phase/Schreiner University/schreiner-university.py
@@ -5,8 +5,8 @@ import csv
 with open('./schreiner-university.txt', 'r', encoding='utf-8') as txt_file:
     txt_content = txt_file.read()
 
-    # Create a regex pattern to match course data
-    pattern = re.compile(r'(\d{4})\.\s*(\(.+?\))?\s*([^0-9]+)(.*?)(?=\d{4}\.|$)', re.DOTALL)
+    # Create a regex pattern to match course data (four digits followed by a period)
+    pattern = re.compile(r'(\d{4})\.?\s+(.*?)\n(.*?)(?=\d{4}\.|$)', re.DOTALL)
 
     # Initialize a list to store extracted course data
     course_data = []
@@ -15,11 +15,7 @@ with open('./schreiner-university.txt', 'r', encoding='utf-8') as txt_file:
     matches = pattern.findall(txt_content)
 
     for match in matches:
-        code, code_in_brackets, title, description = match
-
-        # Ensure the title ends at the end of the line
-        title_lines = title.split('\n')
-        title = title_lines[0].strip()
+        code, title, description = match
 
         # Extract the second digit of the course code as credits
         credits = int(code[1]) if len(code) > 1 and code[1].isdigit() else None
@@ -42,4 +38,4 @@ with open('schreiner-university.csv', 'w', newline='', encoding='utf-8') as csv_
     for course in course_data:
         csv_writer.writerow([course['code'], course['title'], course['credits'], course['description']])
 
-print("CSV file 'schreiner-university' has been created with the extracted course data.")
+print("CSV file 'schreiner-university.csv' has been created with the extracted course data.")
